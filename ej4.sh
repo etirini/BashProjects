8d51f139c9afe81c2a38e4a905ab5e80482caf6d
etirini <etirini@gmail.com>
2022-03-25 21:33:55 -0300

    bash

--- ej4.sh
+++ ej4.sh
@@ -2,14 +2,15 @@
 # Input output
 #para concatenar la ejecucion de un comando contenido en una variable dentro de un echo (ver linea ) al momento de definir la vriable se tiene que colocar el comando entre ``.
 #esto no es necesario si solamente quiero ejecutar el comando desde una variable sin concatenar con texto (como en ej anterior)
-#NO FUNCIONA EN UBUNTU. INVESTIGAR
 
-a = 
+
+coso=`hostname`
+
 echo "hello my name is Tuvieja"
 echo
-#echo "Voquienso"
-#echo
-#read name
+echo "Voquienso"
+echo
+read name
 echo 
-#echo "hola $name"
-echo "el nombre de tu equipo es $a"
+echo "hola $name"
+echo "el nombre del host es $coso"
